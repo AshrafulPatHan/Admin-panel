418885f77654c01e45add12e235e24a5f558351e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-06 15:37:29 +0600

    das

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,8 @@
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export default function Home() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="w-40 mt-9 bg-[#202020] border-[#202020] rounded-none  text-white ">Open</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Keyboard shortcuts
-            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div>
+      j
+    </div>
   )
 }
--- components/navigation/sidebar.tsx
+++ components/navigation/sidebar.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import Logo from "@/public/Logo.png";
+import My from "@/public/my.png";
+import DB from "@/public/dashboard.png";
 // import dL from "@/public/dashboard.png";
 // import { Button, DropdownMenu } from "@radix-ui/themes";
 import { Button } from "@/components/ui/button"
@@ -15,6 +17,8 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { SlArrowDown } from "react-icons/sl";
+
 
 export default function Sidebar() {
    return(
@@ -27,42 +31,22 @@ export default function Sidebar() {
                   <h2 className="font-bold ">Admin Panel Ashraful</h2>
                </div>
                <hr className="w-[210px] mb-4 " />
-               <div>
-                  <DropdownMenu>
-                     <DropdownMenuTrigger asChild>
-                     <Button variant="outline" className="w-[208px] h-[48px] bg-[#202020] border-[#202020] rounded-none 
-                      text-white ">Open</Button>
-                     </DropdownMenuTrigger>
-                     <DropdownMenuContent className="w-56">
-                     <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                     <DropdownMenuSeparator />
-                     <DropdownMenuGroup>
-                        <DropdownMenuItem>
-                           Profile
-                           <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                           Billing
-                           <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                           Settings
-                           <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                           Keyboard shortcuts
-                           <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-                        </DropdownMenuItem>
-                     </DropdownMenuGroup>
-                     </DropdownMenuContent>
-                  </DropdownMenu>
+               <div className="flex items-center gap-3 ml-4 " >
+                  <Image src={My} width={32} height={32} className=" rounded-full" alt="my" />
+                  <h2>Ashraful Pathan</h2>
                </div>
                <hr className="w-[210px] mt-4 mb-4 "/>
                <div className="flex flex-col items-start">
+                  {/* Dashboards --------- */}
                   <DropdownMenu>
                      <DropdownMenuTrigger asChild>
-                     <Button variant="outline" className="w-[208px] h-[48px] bg-[#202020] border-[#202020] rounded-none 
-                      text-white ">Open</Button>
+                     <Button variant="outline" className="w-[208px] justify-evenly px-9 h-[48px] bg-[#58585E] hover:bg-[#202020] 
+                      hover:text-white border-[#202020] rounded-none 
+                      text-white ">
+                        <Image src={DB} width={16} alt="Dash" />
+                        Dashboards
+                        <SlArrowDown />
+                     </Button>
                      </DropdownMenuTrigger>
                      <DropdownMenuContent className="w-56">
                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
@@ -87,11 +71,17 @@ export default function Sidebar() {
                      </DropdownMenuGroup>
                      </DropdownMenuContent>
                   </DropdownMenu>
-                  <button></button>
-                  <button></button>
+                  <button className="w-[206px] justify-evenly px-9 h-[48px] ml-[1.49px] bg-[#E91F63] mt-1 border-[#202020] rounded-none 
+                      text-white ">
+                     <span className="mr-5">A</span> Analytics
+                  </button>
+                  <button className="w-[206px] justify-evenly px-9 h-[48px] ml-[1.49px] bg-[#ffa9a9d0] mt-1 border-[#202020] rounded-none 
+                      text-white ">
+                     <span className="mr-5">S</span> Smart Home
+                  </button>
                </div>
                <div>
-                  <h2>PAGES</h2>
+                  <h2 className="mt-3 text-blue-500">PAGES</h2>
                   <div className="flex flex-col items-start gap-[24px] ml-2 ">
                      <DropdownMenu>
                         <DropdownMenuTrigger asChild>
